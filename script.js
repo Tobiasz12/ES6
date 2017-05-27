7d4471881070f925f13f76e69356a8ca4d23460c
Tobiasz12 <tobiasz63@op.pl>
2017-05-27 18:48:53 +0200

    task1

--- script.js
+++ script.js
@@ -1,12 +1,4 @@
 const sayHello = () => alert('Hello world!');
 sayHello();
 
-let greeting = 'Hello User!';
-function greetWorld(isGreeting) {
-    if (isGreeting) {
-        let greeting = 'Hello World!'; // (A) zasięg: cała funkcja
-        return greeting;
-    }
-    return greeting;
-}
-greetWorld(false); // 'Hello User!'
+//TASK 1
