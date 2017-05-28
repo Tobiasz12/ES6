b259af0a7100b69f2dc8f94d9cec1e02f820fe05
Tobiasz12 <tobiasz63@op.pl>
2017-05-28 15:40:41 +0200

    Task 3.4

--- script-compiled.js
+++ script-compiled.js
@@ -4,3 +4,54 @@ var sayHello = function sayHello() {
   return alert('Hello world!');
 };
 sayHello();
+
+//TASK 1
+function say(y, x) {
+  y = 'Hellow';
+  x = 'World';
+  console.log(y + ' ' + x);
+};
+say();
+
+//TASK 2
+var multiply = function multiply() {
+  var a = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 1;
+  var b = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 1;
+  return a * b;
+};
+multiply(2);
+
+//Task 3
+var arr = [1, 3, 6, 6];
+var a = arr[0],
+    b = arr[1],
+    c = arr[2],
+    rest = arr.slice(3);
+
+
+var average = function average(a) {
+  var b = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 0;
+  var c = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : 0;
+  var d = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : 0;
+  return (a + b + c + d) / (arr.length - rest.length);
+};
+average(1, 3, 6);
+
+//Task4
+var grades = [1, 5, 5, 5, 4, 3, 3, 2, 1];
+var logAllArguments = function logAllArguments() {
+  for (var _len = arguments.length, grades = Array(_len), _key = 0; _key < _len; _key++) {
+    grades[_key] = arguments[_key];
+  }
+
+  return grades.forEach(function (item) {
+    return console.log(item);
+  });
+};
+
+//Task 5
+var array = [1, 4, 'Iwona', false, 'Nowak'];
+var firstName = array[2],
+    lastName = array[4];
+
+console.log(firstName, lastName);
--- script.js
+++ script.js
@@ -1,4 +1,50 @@
 const sayHello = () => alert('Hello world!');
 sayHello();
 
+
 //TASK 1
+function say(y,x) {
+  y = 'Hellow'
+  x = 'World'
+    console.log(`${y} ${x}`);
+};
+say()
+
+//TASK 2
+const multiply = (a = 1,b = 1) => a * b
+multiply(2)
+
+
+//Task 3
+const arr = [1,3,6,6]
+const [a,b,c,...rest] = arr
+
+const average = (a ,b = 0,c = 0 ,d = 0) => (a + b + c + d) / (arr.length - rest.length)
+average(1,3,6)
+
+//Task4
+const grades = [1, 5, 5, 5, 4, 3, 3, 2, 1]
+const logAllArguments = (...grades) => grades.forEach(item => console.log(item));
+
+//Task 5
+const array = [1, 4, 'Iwona', false, 'Nowak'];
+const [, ,firstName, ,lastName] = array;
+console.log(firstName, lastName)
+
+
+
+
+
+
+
+
+
+
+
+
+
+function doStuff (x, y, z) { }
+var args = [0, 1, 2];
+
+// Call the function, passing args
+doStuff.apply(null, args);
